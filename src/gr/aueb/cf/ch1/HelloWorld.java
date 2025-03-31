d9eb1f22164966ebc3dea766d283ee5093a49a92
Th. Androutsos <a8anassis@gmail.com>
2025-03-31 20:24:23 +0300

    feat: chapter 1 created.

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Εμφανίζει τη φράση Hello World στην κονσόλα
+ * (standard output)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
