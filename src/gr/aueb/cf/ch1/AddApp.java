66940ca74832dc587c3204af09df8d05a6c0efee
Th. Androutsos <a8anassis@gmail.com>
2025-04-01 20:41:11 +0300

    fix: printf introduced.

--- src/gr/aueb/cf/ch1/AddApp.java
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -18,5 +18,7 @@ public class AddApp {
 
         // Εκτύπωση αποτελέσματος
         System.out.println("Το αποτέλεσμα είναι: " + result);
+        System.out.println("Το αποτέλεσμα των: " + num1 + " και " + num2 + " είναι ίσο με: " + result);
+        System.out.printf("Το αποτέλεσμα των: %d και %d είναι ίσο με: %d\n", num1, num2, result);
     }
 }
