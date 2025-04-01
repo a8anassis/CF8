7945ae72ce1ca2c3865e8749f07d091b49e96eaa
Th. Androutsos <a8anassis@gmail.com>
2025-04-01 20:28:16 +0300

    fix: comments added.

--- src/gr/aueb/cf/ch1/AddApp.java
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -8,12 +8,15 @@ public class AddApp {
 
     public static void main(String[] args) {
 
+        // Δήλωση και αρχικοποίηση μεταβλητών
         int num1 = 5;
         int num2 = 12;
         int result = 0;
 
+        // Εντολές
         result = num1 + num2;
 
+        // Εκτύπωση αποτελέσματος
         System.out.println("Το αποτέλεσμα είναι: " + result);
     }
 }
