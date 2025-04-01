be4173052ff065b7c21b987679123a48759d2fa1
Th. Androutsos <a8anassis@gmail.com>
2025-04-01 20:05:32 +0300

    feat: add app created.

---
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Προσθέτει δύο ακεραίους και εμφανίζει το
+ * αποτέλεσμα στην κονσόλα.
+ */
+public class AddApp {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 12;
+        int result = 0;
+
+        result = num1 + num2;
+
+        System.out.println("Το αποτέλεσμα είναι: " + result);
+    }
+}
