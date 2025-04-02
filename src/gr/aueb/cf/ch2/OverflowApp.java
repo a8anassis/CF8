52412b804f48b7ff7b261695aaf88242188d5609
Th. Androutsos <a8anassis@gmail.com>
2025-04-02 19:53:29 +0300

    feat: Demo app for overflow on expressions

---
+++ src/gr/aueb/cf/ch2/OverflowApp.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch2;
+
+/**
+ * Προσθέτει δύο ακεραίους αλλά το αποτέλεσμα
+ * δημιουργεί υπερχείλιση (overflow)
+ */
+public class OverflowApp {
+
+    public static void main(String[] args) {
+        int num1 = 2_147_483_647;
+        int num2 = 100;
+        int result = 0;
+
+        result = num1 * num2;
+
+        System.out.println("Το αποτέλεσμα είναι: " + result);
+    }
+}
