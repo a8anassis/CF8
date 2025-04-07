a7c1eef5271bdc7250fe5b3da550f30be8fce66e
Th. Androutsos <a8anassis@gmail.com>
2025-04-07 17:59:19 +0300

    feat: Scanner introduced

---
+++ src/gr/aueb/cf/ch2/ScannerAddApp.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.ch2;
+
+import java.util.Scanner;
+
+/**
+ * Διαβάζει δύο ακεραίους από το
+ * stdin (keyboard) και υπολογίζει το αποτέλεσμα.
+ */
+public class ScannerAddApp {
+
+    public static void main(String[] args) {
+        // Δήλωση και αρχικοποίηση μεταβλητών
+        Scanner in = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
+        int sum = 0;
+
+        // Εντολές
+        System.out.println("Παρακαλώ εισάγετε δύο ακεραίους");
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        sum = num1 + num2;
+
+        // Εκτύπωση Αποτελεσμάτων
+        System.out.printf("Το άθροισμα των %d και %d είναι ίσο με %d", num1, num2, sum);
+    }
+}
