f4e2ab9476f9e337d54daa3ce268132307046c88
Th. Androutsos <a8anassis@gmail.com>
2025-04-07 20:55:35 +0300

    feat: Math random explained

---
+++ src/gr/aueb/cf/ch2/RandomApp.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch2;
+
+/**
+ * Προσομειώνει ένα τυχαίο ζάρι. Δηλαδή παράγει
+ * ένα τυχαίο ακέραιο μεταξύ 1 - 6.
+ */
+public class RandomApp {
+
+    public static void main(String[] args) {
+        int die = 0;
+
+        // (max - min + 1) + min
+        die = (int) (Math.random() * 6) + 1;
+
+        System.out.println(die);
+    }
+}
