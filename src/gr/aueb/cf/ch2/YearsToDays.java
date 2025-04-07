9099f291fb14bcd859d90d3be4e5ccd269d43d58
Th. Androutsos <a8anassis@gmail.com>
2025-04-07 20:27:32 +0300

    feat: years to days created

--- src/gr/aueb/cf/ch2/YearsToDays.java
+++ src/gr/aueb/cf/ch2/YearsToDays.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.ch2;
 
+import java.util.Scanner;
+
 /**
  * Ο χρήστης δίνει την ηλικία του σε έτη (έστω ακέραιος)
  * και το πρόγραμμα επιστρέφει την ηλικία σε ημέρες, όπου
@@ -10,5 +12,15 @@ public class YearsToDays {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        final int DAYS_PER_YEAR = 365;
+        int inputAgeInYears = 0;
+        int ageInDays = 0;
+
+        System.out.println("Παρακαλώ εισάγετε την ηλικία σας");
+        inputAgeInYears = scanner.nextInt();
+        ageInDays = inputAgeInYears * DAYS_PER_YEAR;
+
+        System.out.printf("Ηλικία σε έτη: %d , Ηλικία σε ημέρες: %d", inputAgeInYears, ageInDays);
     }
 }
