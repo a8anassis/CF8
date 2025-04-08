6d74342d61a21a125668f8e5a0ae546d4cb307c4
Th. Androutsos <a8anassis@gmail.com>
2025-04-08 18:46:49 +0300

    feat: frog algorithm added

---
+++ src/gr/aueb/cf/ch2/FrogJump.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch2;
+
+/**
+ * A small frog wants to go to the other side
+ * of the road. The frog is currently at position X
+ * and wants to go to a position equal or greater than Y.
+ * The small frog jumps a fixed distance, D. Find the minimum
+ * number of jumps.
+ *
+ * For example:
+ * X = 10
+ * Y = 85
+ * D = 30
+ *
+ * Jumps -> 3
+ * 10 + 30 = 40
+ * 40 + 30 = 70
+ * 70 + 30 = 100
+ */
+public class FrogJump {
+
+    public static void main(String[] args) {
+        int target = 105;
+        int start = 10;
+        int hop = 30;
+        int jumps = 0;
+
+        jumps = (int) Math.ceil((target - start) / (double) hop);
+
+        System.out.println("Jumps: " + jumps);
+    }
+}
