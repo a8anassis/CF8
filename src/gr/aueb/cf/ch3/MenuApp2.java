3ef244be297fffd71e8b062fac90945ae3d36cb3
Th. Androutsos <a8anassis@gmail.com>
2025-04-09 19:30:58 +0300

    feat: do-while logic emulated with while(true) and sentinel

--- src/gr/aueb/cf/ch3/MenuApp2.java
+++ src/gr/aueb/cf/ch3/MenuApp2.java
@@ -10,8 +10,17 @@ public class MenuApp2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        final int EXIT = 3;
         int choice;
 
-
+        while (true) {
+            System.out.println("Παρακαλώ επιλέξτε ένα από τα παρακάτω:");
+            System.out.println("1. Εισαγωγή Προϊόντος");
+            System.out.println("2. Διαγραφή προϊόντος");
+            System.out.println("3. Έξοδος");
+            choice = scanner.nextInt();
+            if (choice == EXIT) break;
+        }
+        System.out.println("Ευχαριστούμε για τη χρήση της εφαρμογής!!");
     }
 }
