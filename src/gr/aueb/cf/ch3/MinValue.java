d4657e6395d421f9f58f6ec17cbeb12cff389a9b
Th. Androutsos <a8anassis@gmail.com>
2025-04-09 20:47:25 +0300

    feat: Ternary operator added

---
+++ src/gr/aueb/cf/ch3/MinValue.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.ch3;
+
+import java.util.Scanner;
+
+/**
+ * Βρίσκει την μικρότερη τιμή μεταξύ δύο ακεραίων.
+ */
+public class MinValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        int num2;
+        int min;
+
+        System.out.println("Παρακαλώ εισάγετε δύο ακεραίους");
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+
+//        if (num1 < num2) {
+//            min = num1;
+//        } else {
+//            min = num2;
+//        }
+
+        min = (num1 < num2) ? num1 : num2;
+    }
+}
