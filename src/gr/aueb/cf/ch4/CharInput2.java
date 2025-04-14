8d8d7a1d614adf4c1e7c4c18ead0a6409bb8bd31
Th. Androutsos <a8anassis@gmail.com>
2025-04-14 20:35:24 +0300

    feat: std input with Scanner

---
+++ src/gr/aueb/cf/ch4/CharInput2.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch4;
+
+import java.util.Scanner;
+
+/**
+ * Διαβάζει char με Scanner.
+ */
+public class CharInput2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char inputChar = ' ';
+
+        System.out.println("Παρακαλώ δώστε ένα χαρακτήρα");
+        inputChar = scanner.nextLine().charAt(0);
+        System.out.println("Input char: " + inputChar);
+    }
+}
