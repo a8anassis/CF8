73a53945d064880322375dfeab97dcb5306dc705
Th. Androutsos <a8anassis@gmail.com>
2025-04-14 20:48:15 +0300

    feat: sum and mul with for

---
+++ src/gr/aueb/cf/ch4/SumMulApp.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch4;
+
+/**
+ * Υπολογίζει το άθροισμα και το γινόμενο
+ * των 10 πρώτων ακεραίων.
+ */
+public class SumMulApp {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int mul = 1;
+
+        for (int i = 1; i <= 10; i++) {
+            sum += i;   // sum = sum + i
+            mul *= i;   // mul = mul * i
+        }
+
+        System.out.println("Sum is: " + sum);
+        System.out.println("Mul is: " + mul);
+    }
+}
