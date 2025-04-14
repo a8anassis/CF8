8c676ff63a4671368565014d907a0fe2fd86f6e5
Th. Androutsos <a8anassis@gmail.com>
2025-04-14 20:25:26 +0300

    fix: new Unicode symbols were added

--- src/gr/aueb/cf/ch4/UnicodeApp.java
+++ src/gr/aueb/cf/ch4/UnicodeApp.java
@@ -8,6 +8,10 @@ public class UnicodeApp {
     public static void main(String[] args) {
         int codePoint = 0x1F60A;    // U+D83D U+DE03, smiley
         System.out.println(Character.toChars(codePoint));
-        System.out.println("\u20ac");   // euro sign
+        System.out.println(Character.toChars(0x1F642)); // smiling face
+        System.out.println("\u20ac");   // Euro sign
+        System.out.println("\u00A9");   // Copyright sign
+        System.out.println("\u2103");   // Celsius sign
+        System.out.println("\u2109");   // Fahrenheit sign
     }
 }
