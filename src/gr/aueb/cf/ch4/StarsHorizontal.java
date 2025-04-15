7ae075070ab8668b482b65a6cdfdc40072497a73
Th. Androutsos <a8anassis@gmail.com>
2025-04-15 19:41:00 +0300

    feat: horizontal stars added

---
+++ src/gr/aueb/cf/ch4/StarsHorizontal.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch4;
+
+/**
+ * Εκτυπώνει 10 οριζόντια αστεράκια.
+ */
+public class StarsHorizontal {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print('*');
+        }
+    }
+}
