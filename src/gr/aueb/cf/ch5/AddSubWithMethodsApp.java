ebbbebc7eff59f3f776f5dd08d108cd60ea1abf6
Th. Androutsos <a8anassis@gmail.com>
2025-04-16 20:24:49 +0300

    feat: methods were introduced with add and sub

--- src/gr/aueb/cf/ch5/AddSubWithMethodsApp.java
+++ src/gr/aueb/cf/ch5/AddSubWithMethodsApp.java
@@ -6,21 +6,24 @@ import java.util.Scanner;
  * Υπολογίζει το άθροισμα δύο ακεραίων
  * με τη χρήση μεθόδων.
  */
-public class AddApp {
+public class AddSubWithMethodsApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1;
         int num2;
         int sum;
+        int sub;
 
         System.out.println("Παρακαλώ εισάγετε δύο ακεραίους");
         num1 = scanner.nextInt();
         num2 = scanner.nextInt();
 
         sum = add(num1, num2);      // invoke - call the add method
+        sub = AddSubWithMethodsApp.sub(num2, num1);
 
         System.out.println("Sum: " + sum);
+        System.out.println("Sub: " + sub);
     }
 
     /**
@@ -32,6 +35,17 @@ public class AddApp {
      */
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    /**
+     * Subtracts two integers.
+     *
+     * @param a     the first integer to subtract from.
+     * @param b     the second integer that is subtracted.
+     * @return      the subtraction of a - b.
+     */
+    public static int sub(int a, int b) {
+        return a - b;
     }
 
 //    public static int add(int a, int b) {
