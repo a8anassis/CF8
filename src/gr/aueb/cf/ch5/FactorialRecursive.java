c50a8bce1835b0a089e0aa3a2c2e230b4bac9cb4
Th. Androutsos <a8anassis@gmail.com>
2025-04-23 20:41:38 +0300

    feat: Factorial Recursive

---
+++ src/gr/aueb/cf/ch5/FactorialRecursive.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch5;
+
+/**
+ * Calculates n! recursively.
+ */
+public class FactorialRecursive {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(facto(i));
+        }
+    }
+
+    public static int facto(int n) {
+        if (n <= 1) return 1;
+        return n * facto(n - 1);    // Recursion
+    }
+}
