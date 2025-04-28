2329ffe2d1941a9ea1df5560a3ea085fa66570c2
Th. Androutsos <a8anassis@gmail.com>
2025-04-28 19:42:09 +0300

    feat: array traverse with method.

---
+++ src/gr/aueb/cf/ch6/ArraysAndMethods.java
@@ -0,0 +1,47 @@
+package gr.aueb.cf.ch6;
+
+/**
+ * Demo with arrays and methods.
+ * Πως περνάμε πίνακες ως παραμέτρους
+ * μεθόδους.
+ */
+public class ArraysAndMethods {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Prints the elements of an array.
+     *
+     * @param arr   the input array.
+     */
+    public static void printArray(int[] arr) {
+        arr[0] = 1000;  // Οποιαδήποτε αλλαγή στις τιμές του array διατηρείται.
+        for (int el : arr) {
+            System.out.print(el + " ");
+        }
+    }
+
+
+    /**
+     * Overloaded version of for with boundaries.
+     * Print an array slice from 'low' to 'high',
+     * that is from low till high - 1. High is not
+     * inclusive. Checks for edge cases: low < 0
+     * and high > length - 1.
+     *
+     * @param arr       the input array.
+     * @param low       the low index.
+     * @param high      the high index (exclusive)
+     */
+    public static void printArray(int[] arr, int low, int high) {
+        if (low < 0 || high > arr.length) {
+            return;
+        }
+
+        for (int i = low; i < high; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
