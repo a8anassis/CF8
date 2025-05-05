d9a88e61422a6ef9cd507264dba73ed8289ae8d1
Th. Androutsos <a8anassis@gmail.com>
2025-05-05 20:52:43 +0300

    fix: String concat

---
+++ src/gr/aueb/cf/ch7/StringConcat.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch7;
+
+/**
+ * Συνενώνει δύο ή περισσότερα Strings.
+ */
+public class StringConcat {
+
+    public static void main(String[] args) {
+        String s1 = "Coding";
+        String s2 = "Factory";
+
+        String s3 = s1 + s2;    // + operator in Java is overloaded
+        String s4 = s1.concat(s2);
+
+        String s5 = s1 + " " + s2;
+        String s6 = s1.concat(" ").concat(s2);  // chaining
+    }
+}
