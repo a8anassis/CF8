b4f3ae2732733856a05eb068a71eb3ccbc93e48e
Th. Androutsos <a8anassis@gmail.com>
2025-05-07 18:58:12 +0300

    feat: Number format exception added

---
+++ src/gr/aueb/cf/ch8/ExceptionNumberFormat.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.ch8;
+
+import java.util.Scanner;
+
+/**
+ * Συμβαίνει όταν αποτυγχάνει η {@link Integer#parseInt(String)}.
+ */
+public class ExceptionNumberFormat {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        String inputStr = "";
+
+        System.out.println("Παρακαλώ εισάγετε ένα ακέραιο");
+
+        while (!isInteger(inputStr = scanner.nextLine())) {
+            System.out.println("Οι χαρακτήρες είναι μη αποδεκτοί. Παρακαλώ δώστε νέο ακέραιο");
+        }
+
+        num1 = Integer.parseInt(inputStr);
+        System.out.println(num1);
+    }
+
+    public static boolean isInteger(String s) {
+        return s.matches("-?\\d+");
+    }
+}
