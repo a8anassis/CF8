1ca0df6f2a9f3e7417270fa13ede0131d23dfdc7
a8anassis <a8anassis@gmail.com>
2025-05-12 13:17:14 +0300

    feat: file writer added.

--- src/gr/aueb/cf/ch9/FileWritersApp.java
+++ src/gr/aueb/cf/ch9/FileWritersApp.java
@@ -1,5 +1,6 @@
 package gr.aueb.cf.ch9;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,19 +9,30 @@ import java.time.LocalDateTime;
 public class FileWritersApp {
 
     public static void main(String[] args) {
-        File fd = new File("C:/tmp/file-writer.txt");
+        File fd = new File("C:/tmp/file-writer3.txt");
         try {
-            fileWriter(fd);
+//            fileWriter(fd);
+            bufferedWriter(fd);
         } catch (IOException e) {
             System.out.println("Το αρχείο δεν δημιουργήθηκε ή κάποιο λάθος συνέβη.");
         }
     }
 
-
     public static void fileWriter(File file) throws IOException {
-        try (FileWriter fw = new FileWriter(file)) {
+        try (FileWriter fw = new FileWriter(file, true)) {      // true is append
             fw.write("Coding!");
             fw.flush();
+        } catch (IOException e) {
+            System.out.println(LocalDateTime.now() + "\n" + e);
+            throw e;
+        }
+    }
+
+    public static void bufferedWriter(File file) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+            bw.write("Coding!");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             System.out.println(LocalDateTime.now() + "\n" + e);
             throw e;
