509e217894eba533167c294fbfae63d263f3789e
a8anassis <a8anassis@gmail.com>
2025-05-12 18:48:09 +0300

    feat: add more writer classes
    PrintWriter, Print Stream.

--- src/gr/aueb/cf/ch9/FileWritersApp.java
+++ src/gr/aueb/cf/ch9/FileWritersApp.java
@@ -1,9 +1,7 @@
 package gr.aueb.cf.ch9;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 public class FileWritersApp {
@@ -11,19 +9,20 @@ public class FileWritersApp {
     public static void main(String[] args) {
         File fd = new File("C:/tmp/file-writer3.txt");
         try {
-//            fileWriter(fd);
+            fileWriter("C:/tmp/file-writer3.txt");
             bufferedWriter(fd);
         } catch (IOException e) {
             System.out.println("Το αρχείο δεν δημιουργήθηκε ή κάποιο λάθος συνέβη.");
         }
     }
 
-    public static void fileWriter(File file) throws IOException {
+    public static void fileWriter(String file) throws IOException {
         try (FileWriter fw = new FileWriter(file, true)) {      // true is append
             fw.write("Coding!");
             fw.flush();
-        } catch (IOException e) {
-            System.out.println(LocalDateTime.now() + "\n" + e);
+        }
+        catch (IOException e) {
+            System.err.println(LocalDateTime.now() + "\n" + e);
             throw e;
         }
     }
@@ -33,6 +32,17 @@ public class FileWritersApp {
             bw.write("Coding!");
             bw.newLine();
             bw.flush();
+        } catch (IOException e) {
+            System.out.println(LocalDateTime.now() + "\n" + e);
+            throw e;
+        }
+    }
+
+    public static void printStream(String file) throws IOException {
+//        try (PrintStream ps = new PrintStream(file, StandardCharsets.UTF_8)) {
+        try (PrintStream ps = new PrintStream(new FileOutputStream(file, true), false, StandardCharsets.UTF_8)) {   // true is append
+           ps.println("Printing with print stream");
+           ps.flush();
         } catch (IOException e) {
             System.out.println(LocalDateTime.now() + "\n" + e);
             throw e;
