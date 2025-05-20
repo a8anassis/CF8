3a30b5eaa1ab989200889f5843dc0dad2fde2668
a8anassis <a8anassis@gmail.com>
2025-05-20 19:28:01 +0300

    feat: add Student class with private fields, getters and setters

--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -4,7 +4,31 @@ package gr.aueb.cf.ch12;
  * Very simple data class.
  */
 public class Student {
-    public int id;
-    public String firstname;
-    public String lastname;
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
