d5bcbc11195eea58e6a4ded0451aa55610b0176e
a8anassis <a8anassis@gmail.com>
2025-05-20 19:59:11 +0300

    feat: add overloaded constructor

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -4,14 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student alice = new Student();
+        Student alice = new Student();      // id -> 0, firstname -> null, lastname -> null
+        Student bob = new Student(2, "Bob", "D.");  // id -> 2, firstname -> Bob, lastname -> D.
 
-        alice.id = 10;                  // set id
-        alice.firstname = "Alice";      // set firstname
-        alice.lastname = "W.";          // set lastname
+        System.out.println("Bob ID: " + bob.getId());       // 2
 
-        System.out.println("ID: " + alice.id);                  // get id
-        System.out.println("Firstname: " + alice.firstname);    // get firstname
-        System.out.println("Lastname: " + alice.lastname);      // get lastname
+        bob.setId(23);
+
+        System.out.println("Bob ID: " + bob.getId());       // 23
+
+        alice.setId(10);                  // set id
+        alice.setFirstname("Alice");      // set firstname
+        alice.setLastname("W.");          // set lastname
+
+        System.out.println("ID: " + alice.getId());                  // get id
+        System.out.println("Firstname: " + alice.getFirstname());    // get firstname
+        System.out.println("Lastname: " + alice.getLastname());      // get lastname
+
+        bob.setLastname("Dylan");
     }
 }
--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -2,11 +2,24 @@ package gr.aueb.cf.ch12;
 
 /**
  * Very simple data class.
+ * private fields, Default Constructor και
+ * getters και setters με convention
+ * ονομάζονται JavaBean.
  */
 public class Student {
     private int id;
     private String firstname;
     private String lastname;
+
+    public Student() {
+
+    }
+
+    public Student(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 
     public int getId() {
         return id;
