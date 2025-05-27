2b7500b9870b0154ee38b9444b3001f6f04e3bb4
a8anassis <a8anassis@gmail.com>
2025-05-27 19:37:02 +0300

    feat: add quizzes in classes and inheritance

---
+++ src/gr/aueb/cf/ch15/quiz/quiz3/B.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch15.quiz.quiz3;
+
+public class B extends A {
+
+    public B() {
+        super();
+        System.out.println("B");
+    }
+}
---
+++ src/gr/aueb/cf/ch15/quiz/quiz5/Child5.java
@@ -0,0 +1,9 @@
+//package gr.aueb.cf.ch15.quiz.quiz5;
+//
+//public class Child5 extends Parent5 {
+//
+//    public Child5() {
+//
+//        System.out.println("Child Constructor");
+//    }
+//}
