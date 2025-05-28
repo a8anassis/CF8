b71cf56d8fe6c17e0d54a2f1e960f1661f197e0e
a8anassis <a8anassis@gmail.com>
2025-05-28 18:32:48 +0300

    feat: add quiz 1

---
+++ src/gr/aueb/cf/ch16/challenges/quiz1/Dog.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch16.challenges.quiz1;
+
+public class Dog extends Animal {
+
+//    public Dog() {
+//        super();
+//    }
+
+    @Override
+    public void talk() {
+        System.out.println("Dog says hi!");
+    }
+}
