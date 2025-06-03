42bea6dc4a53eab3348f5c2b484bbfd841e6d431
a8anassis <a8anassis@gmail.com>
2025-06-03 19:58:40 +0300

    feat: add lifo parking

---
+++ src/gr/aueb/cf/ch19/lifo_parking/LifoParking.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.ch19.lifo_parking;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Υλοποιεί τη λογική ενός parking με λογική
+ * Last-In-First-Out.
+ */
+public class LifoParking {
+    private final Deque<String> parking = new LinkedList<>();
+
+    public LifoParking() {
+
+    }
+
+    public List<String> getParking() {
+        return List.copyOf(parking);
+        //return Collections.unmodifiableList(parking);
+    }
+
+    // Delegation
+    public void addCar(String carId) {
+        parking.push(carId);
+        System.out.println(LocalDateTime.now() + ", Είσοδος, Αρ. Πινακίδας: " + carId);
+    }
+
+    public String removeCar() {
+        String carId = parking.pop();
+        System.out.println(LocalDateTime.now() + ", Έξοδος, Αρ. Πινακίδας: " + carId);
+        return carId;
+    }
+
+    public boolean isEmpty() {
+        return parking.isEmpty();
+    }
+
+    public void traverse() {
+        parking.forEach(System.out::println);
+    }
+}
+
