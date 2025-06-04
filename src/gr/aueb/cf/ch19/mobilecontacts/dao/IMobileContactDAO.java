d3e03b27f81b1c2a89be943f9e1a80c00f74d285
a8anassis <a8anassis@gmail.com>
2025-06-04 20:28:02 +0300

    feat: add MobileContact dao interface

---
+++ src/gr/aueb/cf/ch19/mobilecontacts/dao/IMobileContactDAO.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch19.mobilecontacts.dao;
+
+import gr.aueb.cf.ch19.mobilecontacts.model.MobileContact;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IMobileContactDAO {
+    MobileContact insert(MobileContact mobileContact);
+    MobileContact update(long id, MobileContact mobileContact);
+    void deleteById(long id);
+    MobileContact getById(long id);
+    List<MobileContact> getAll();
+
+    void deleteByPhoneNumber(String phoneNumber);
+    Optional<MobileContact> getByPhoneNumber(String phoneNumber);
+    boolean contactIdExists(long id);
+    boolean phoneNumberExists(String phoneNumber);
+}
