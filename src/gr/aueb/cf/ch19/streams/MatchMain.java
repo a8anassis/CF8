3c6b6875697910845b501d0bd784d86f388daa13
a8anassis <a8anassis@gmail.com>
2025-06-04 19:58:23 +0300

    fix: add stream logic with match

---
+++ src/gr/aueb/cf/ch19/streams/MatchMain.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch19.streams;
+
+import java.util.List;
+
+public class MatchMain {
+
+    public static void main(String[] args) {
+        List<String> names = List.of("Alice", "Bob", "Costas", "Denis");
+
+        boolean isDenisExists = names.stream()
+                .anyMatch(name -> name.equals("Denis"));
+
+        boolean allAreDenis = names.stream()
+                .allMatch(name -> name.equals("Denis"));
+
+        boolean noneIsDenis = names.stream()
+                .noneMatch(name -> name.equals("Denis"));
+    }
+}
