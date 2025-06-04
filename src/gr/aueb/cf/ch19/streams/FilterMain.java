ca75d2aa150179f38c7411a85017f9151b6af7de
a8anassis <a8anassis@gmail.com>
2025-06-04 18:57:13 +0300

    feat: add filter logic with streams

---
+++ src/gr/aueb/cf/ch19/streams/FilterMain.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch19.streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterMain {
+
+    public static void main(String[] args) {
+        List<String> cities = List.of("Athens", "Toronto", "Tunis", "Athens", "London");
+
+        var filteredCities = cities.stream()
+                .filter(city -> city.startsWith("T"))   // Predicate = boolean function
+                .toList();  // immutable list
+
+        var filteredCities2 = cities.stream()
+                .filter(city -> city.startsWith("T"))
+                .collect(Collectors.toList());  // mutable list
+    }
+}
