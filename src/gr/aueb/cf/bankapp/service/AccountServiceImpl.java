01eda35b6ed43f49b75608f48fbdd2cc211de9de
a8anassis <a8anassis@gmail.com>
2025-06-11 15:46:55 +0300

    feat: add services

---
+++ src/gr/aueb/cf/bankapp/service/AccountServiceImpl.java
@@ -0,0 +1,63 @@
+package gr.aueb.cf.bankapp.service;
+
+import gr.aueb.cf.bankapp.dao.AccountDAOImpl;
+import gr.aueb.cf.bankapp.dao.IAccountDAO;
+import gr.aueb.cf.bankapp.dto.AccountInsertDTO;
+import gr.aueb.cf.bankapp.dto.AccountReadOnlyDTO;
+import gr.aueb.cf.bankapp.exceptions.AccountNotFoundException;
+import gr.aueb.cf.bankapp.exceptions.InsufficientBalanceException;
+import gr.aueb.cf.bankapp.exceptions.NegativeAmountException;
+import gr.aueb.cf.bankapp.model.Account;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class AccountServiceImpl implements IAccountService {
+    private final IAccountDAO accountDAO;
+
+    public AccountServiceImpl(IAccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    @Override
+    public boolean createNewAccount(AccountInsertDTO dto) {
+        return false;
+    }
+
+    @Override
+    public void deposit(String iban, BigDecimal amount) throws NegativeAmountException, AccountNotFoundException {
+        try {
+            Account account = accountDAO.getByIban(iban)
+                    .orElseThrow(() -> new AccountNotFoundException("Account with iban " + iban + " not found."));
+
+            if (amount.compareTo(BigDecimal.ZERO) < 0) {
+                throw new NegativeAmountException("Invalid amount: " + amount + ". Amount must be positive (input was negative)");
+            }
+
+            account.setBalance(account.getBalance().add(amount));
+            accountDAO.saveOrUpdate(account);
+        } catch (NegativeAmountException e) {
+            System.err.printf("%s. The amount=%f is negative. \n%s", LocalDateTime.now(), amount, e);
+            throw e;
+        } catch (AccountNotFoundException e) {
+            System.err.printf("%s. The account with iban=%s not found. \n%s", LocalDateTime.now(), iban, e);
+            throw e;
+        }
+    }
+
+    @Override
+    public void withdraw(String iban, BigDecimal amount) throws NegativeAmountException, InsufficientBalanceException, AccountNotFoundException {
+
+    }
+
+    @Override
+    public BigDecimal getBalance(String iban) throws AccountNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<AccountReadOnlyDTO> getAccounts() {
+        return List.of();
+    }
+}
