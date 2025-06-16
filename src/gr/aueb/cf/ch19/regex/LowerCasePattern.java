566017678891d3879ff91befea4f72f66be5647d
a8anassis <a8anassis@gmail.com>
2025-06-16 20:57:39 +0300

    feat: add zero-length assertions

---
+++ src/gr/aueb/cf/ch19/regex/LowerCasePattern.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch19.regex;
+
+import java.nio.file.Pathimport java.util.regex.Pattern;
+
+/**
+ * Κάνει match 8 τουλάχιστον χαρακτήρες εκ των οποίων
+ * τουλάχιστον ένας είναι lower-case.
+ */
+public class LowerCasePattern {
+
+    public static void main(String[] args) {
+        String s = "bU34&#PP";
+
+        Pattern pattern = Pattern.compile("(?=.*?[a-z])(?=.*?[A-Z])^.{8,}$");
+    }
+}
